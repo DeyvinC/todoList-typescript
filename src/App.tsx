887b423481569ef79d9c7e87216e5485f64d35a1
DeyvinC <deyvinvelasquez0202@gmail.com>
2022-09-08 16:47:30 -0400

    todo list using react and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "../src/components/InputField";
+import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
-function App() {
+let name: string;
+let age: number | string; // the straight line means in typescript that it can be either or.
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string]; // tupild
+
+let printName: (name: string) => void; // void returns undefined and never returns nothing
+
+// function printName(name: string) {
+//   console.log(name)
+// }
+// printName("Deyvin")
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+
+// let person : Person = {
+//   name: "Deyvin",
+//   // age: 22
+// }
+
+// let lotsOfPeople: Person[]
+
+// let personName: unknown;
+
+// interface Person {
+//   name: string;
+//   age?: number;
+// }
+
+// interface Guy extends Person {
+//   profession: string
+// }
+
+// type X = {
+//   a:string;
+//   b: number
+// }
+
+// type Y = X & {
+//   c:string;
+//   d: number
+// }
+
+// let y : Y = {
+//   a:'gfjgh',
+//   b: 2367,
+//   c: "adkkf",
+//   d: 356
+// }
+
+// React.ReactNode supports all of them
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  // console.log(todos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Todo } from "../model";
+import { AiFillEdit } from "react-icons/ai";
+import { AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./styles.css";
+import TodoList from "./TodoList";
+
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
+  return (
+    <form className="todosSingle" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todosSingle--text"
+        />
+      ) : todo.isDone ? (
+        <s className="todosSingle--text">{todo.todo}</s>
+      ) : (
+        <span className="todosSingle--text">{todo.todo}</span>
+      )}
+
+      <div>
+        <span className="icon">
+          <AiFillEdit
+            onClick={() => {
+              if (!edit && !todo.isDone) {
+                setEdit(!edit);
+              }
+            }}
+          />
+        </span>
+        <span className="icon">
+          <AiFillDelete onClick={() => handleDelete(todo.id)} />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
+export default SingleTodo;
